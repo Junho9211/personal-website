141d195020629b8254484cb58d2c71b31cb1960a
junho.kim <junko.kim@nvinteractive.com>
2025-07-15 16:19:26 +1200

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(), // Integrate the Tailwind CSS Vite plugin
+  ],
+  build: {
+    outDir: 'dist',
+  },
+});
